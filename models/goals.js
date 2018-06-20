610e99d7ae15762f4cb60f2f984a28c83c184a5d
Nate <nathanperfetti@gmail.com>
2018-06-20 16:48:00 -0500

    Update controller and orm completed

--- models/goals.js
+++ models/goals.js
@@ -12,6 +12,11 @@ const goal = {
         orm.create("burgers", cols, vals, function(res) {
           cb(res);
         });
+      },
+      update: function(objColVals, condition, cb) {
+        orm.update("burgers", objColVals, condition, function(res) {
+          cb(res);
+        });
       }
 
 
